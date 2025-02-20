653b0c322c208e3e61ddeb8e9d6c502a7bbca05d
BhanuTMD <bhanupratapdwivedi1306@gmail.com>
2025-02-20 11:54:28 +0530

    Data Service add in 1st commit
    This is my first commit of version in this TDMP project of TMD I have created some Get Post and Update API of section 1.

---
+++ TDMP/src/main/java/com/CSIR/TMD/ApfApplication.java
@@ -0,0 +1,13 @@
+package com.CSIR.TMD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApfApplication.class, args);
+	}
+
+}
